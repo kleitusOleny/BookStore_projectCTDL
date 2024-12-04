ee762cc4e2ff5c239a2a14fc892693f517b0a85e
Oleny KleiTus <olenyDev@gmail.com>
2024-12-05 00:46:23 +0700

    Big demo update

--- src/BookStore/util/FileLoader.java
+++ src/BookStore/util/FileLoader.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class FileLoader {
     public Set loadBook() throws IOException{
         Book book = null;
-        Set<Book> set = new HashSet<>();
+        Set<Book> setBook = new HashSet<>();
         String file = "src/data/listbook.txt";
         BufferedReader br = new BufferedReader(new FileReader(file));
         
@@ -20,12 +20,28 @@ public class FileLoader {
         while ((line = br.readLine()) != null) {
             String[] str = line.split("\\|");
             book = new Book(str[0].trim(),str[1].trim(),Double.parseDouble(str[2].trim()),str[3].trim(),str[4].trim(),str[5].trim(),Integer.parseInt(str[6].trim()),Integer.parseInt(str[7].trim()));
-            set.add(book);
+            setBook.add(book);
         }
         br.close();
-        return set;
+        return setBook;
     }
     
+    public Set<Customer> loadCustomer() throws IOException{
+        Customer customer = null;
+        Set<Customer> setCustomer = new HashSet<>();
+        String file = "src/data/acc.txt";
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        
+        String line ="";
+        
+        while ((line = br.readLine()) != null) {
+            String[] str = line.split("\\|");
+            customer = new Customer(str[0].trim(),str[1].trim(),Integer.parseInt(str[2].trim()));
+            setCustomer.add(customer);
+        }
+        br.close();
+        return setCustomer;
+    }
     public static void main(String[] args) throws IOException {
         FileLoader loader = new FileLoader();
         System.out.println(loader.loadBook());
