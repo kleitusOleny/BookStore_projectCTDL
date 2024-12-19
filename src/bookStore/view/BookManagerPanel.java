51728fe4d5f3df287956cd912918a107bd4459f3
Oleny KleiTus <olenyDev@gmail.com>
2024-12-19 16:32:45 +0700

    Fix giao dien quan li don hang

--- src/bookStore/view/BookManagerPanel.java
+++ src/bookStore/view/BookManagerPanel.java
@@ -1,9 +1,11 @@
 package bookStore.view;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class BookManagerPanel extends JPanel {
+    JTextField textIDBook,textTitle,textAuthor, textPulish, textPrice, textYearPub;
     public BookManagerPanel(){
         setLayout(new BorderLayout());
         
@@ -14,16 +16,46 @@ public class BookManagerPanel extends JPanel {
         
     }
    
-    public static class InfoPanel extends JPanel{
+    public class InfoPanel extends JPanel{
         public InfoPanel(){
             setLayout(new BorderLayout());
-            JLabel titleLabel = new JLabel("Quản Lí Sách",JLabel.CENTER);
-            titleLabel.setFont(new Font("Arial",Font.BOLD,20));
-            add(titleLabel,BorderLayout.NORTH);
+            TitledBorder titledBorder = new TitledBorder("Quản Lí Sách");
+            titledBorder.setTitleFont(new Font("Arial",Font.BOLD,20));
+            setBorder(titledBorder);
             
-            JPanel insertInfoPanel = new JPanel();
-            insertInfoPanel.setLayout(new GridLayout(8,2));
+            JPanel insertInfoPanel = new JPanel(new GridLayout(6,2));
+            JLabel idBookLabel = new JLabel("Mã Sách");
+            textIDBook = new JTextField();
             
+            insertInfoPanel.add(idBookLabel);
+            insertInfoPanel.add(textIDBook);
+            
+            JLabel titleLabel = new JLabel("Tên Sách");
+            textTitle = new JTextField();
+            insertInfoPanel.add(titleLabel);
+            insertInfoPanel.add(textTitle);
+            
+            JLabel authorLabel = new JLabel("Tên Tác Giả");
+            textAuthor = new JTextField();
+            insertInfoPanel.add(authorLabel);
+            insertInfoPanel.add(textAuthor);
+            
+            JLabel publishLabel = new JLabel("Nhà Xuất Bản");
+            textPulish = new JTextField("");
+            insertInfoPanel.add(publishLabel);
+            insertInfoPanel.add(textPulish);
+            
+            JLabel priceLabel = new JLabel("Giá Sách");
+            textPrice = new JTextField("");
+            insertInfoPanel.add(priceLabel);
+            insertInfoPanel.add(textPrice);
+            
+            JLabel yearPubLabel = new JLabel("Năm Xuất Bản: ");
+            textYearPub = new JTextField();
+            insertInfoPanel.add(yearPubLabel);
+            insertInfoPanel.add(textYearPub);
+            
+            add(insertInfoPanel);
         }
     }
     
